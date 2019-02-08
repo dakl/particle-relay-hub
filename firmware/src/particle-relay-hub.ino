f470e8a8cffe74d280934f157e4b907e4fab9001
Daniel Klevebring <daniel.klevebring@gmail.com>
2019-02-08 15:47:42 +0100

    add basic mqtt callback

--- firmware/src/particle-relay-hub.ino
+++ firmware/src/particle-relay-hub.ino
@@ -9,6 +9,19 @@
 // particle-relay-hub
 // ------------
 
+#include "MQTT.h"
+
+/* ======================= prototypes =============================== */
+
+void callback(char *topic, byte *payload, unsigned int length);
+
+/* ======================= main.ino ======================== */
+
+MQTT client("worker0", 1883, callback);
+
+char *COMMANDS_TOPICS = "commands/relay/1";
+char *EVENTS_TOPIC_TEMPLATE = "events/relay/";
+
 int RELAY1 = D3;
 int RELAY2 = D4;
 int RELAY3 = D5;
@@ -18,20 +31,31 @@ int STATES[] = {0, 0, 0, 0};
 
 int ttl = 16777215;
 
+void callback(char *topic, byte *payload, unsigned int length)
+{
+    char p[length + 1];
+    memcpy(p, payload, length);
+    p[length] = NULL;
+    Particle.publish("event_callback", "handing payload: " + String(p), ttl);
+    client.publish(String(EVENTS_TOPIC_TEMPLATE) + "1", p);
+    client.publish(String(EVENTS_TOPIC_TEMPLATE) + "1", "{}");
+}
+
 void reset_handler()
 {
     // tell the world what we are doing
     Particle.publish("reset", "going down for reboot NOW!", ttl);
 }
 
-
 int getRelayNumber(String command)
 {
     // parse the relay number
     int relayNumber = command.charAt(1) - '0';
     // do a sanity check
-    if (relayNumber < 1 || relayNumber > 4) return -1;
-    else return relayNumber;
+    if (relayNumber < 1 || relayNumber > 4)
+        return -1;
+    else
+        return relayNumber;
 }
 
 /*
@@ -48,18 +72,22 @@ int relayControl(String command)
     int relayState = 0;
     // parse the relay number
     int relayNumber = getRelayNumber(command);
-    if (relayNumber == -1) return -1;
+    if (relayNumber == -1)
+        return -1;
 
     // find out the state of the relay
-    if (command.substring(3,7) == "HIGH") relayState = 1;
-    else if (command.substring(3,6) == "LOW") relayState = 0;
-    else return -1;
+    if (command.substring(3, 7) == "HIGH")
+        relayState = 1;
+    else if (command.substring(3, 6) == "LOW")
+        relayState = 0;
+    else
+        return -1;
 
     // write to the appropriate relay
-    digitalWrite(relayNumber+2, relayState);
+    digitalWrite(relayNumber + 2, relayState);
 
     // update states variable
-    STATES[relayNumber-1] = relayState;
+    STATES[relayNumber - 1] = relayState;
 
     return 1;
 }
@@ -77,12 +105,14 @@ int relayState(String args)
 {
     // parse the relay number
     int relayNumber = getRelayNumber(args);
-    if (relayNumber == -1) return -1;
+    if (relayNumber == -1)
+        return -1;
 
-    return STATES[relayNumber-1];
+    return STATES[relayNumber - 1];
 }
 
-void ready() {
+void ready()
+{
     // code for displaying a notification that the system is ready
     // for example blinking each thing once
     Particle.publish("booted", "particle-relay-hub is ready.", ttl);
@@ -107,14 +137,27 @@ void setup()
     digitalWrite(RELAY3, LOW);
     digitalWrite(RELAY4, LOW);
 
-    // register control function
+    // register functions
     Particle.function("relay", relayControl);
-
-    // register state function
     Particle.function("state", relayState);
+
+    // // connect to the mqtt broker
+    client.connect("sparkclient");
+
+    // publish/subscribe
+    if (client.isConnected())
+    {
+        Particle.publish("MQTT: connected", PRIVATE);
+
+        client.publish(String(EVENTS_TOPIC_TEMPLATE)+"1", "Online.");
+        client.subscribe(COMMANDS_TOPICS);
+    }
 
     ready();
 }
 
-void loop() {
+void loop()
+{
+    if (client.isConnected())
+        client.loop();
 }
