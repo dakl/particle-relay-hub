9d0b269336c5a1e00e963892dc0422e045976b15
Daniel Klevebring <daniel.klevebring@gmail.com>
2019-02-11 10:08:32 +0100

    make nice

--- firmware/src/particle-relay-hub.ino
+++ firmware/src/particle-relay-hub.ino
@@ -14,6 +14,7 @@
 /* ======================= prototypes =============================== */
 
 void callback(char *topic, byte *payload, unsigned int length);
+int setState(int relayNumber, int relayState);
 
 /* ======================= main.ino ======================== */
 
@@ -27,23 +28,32 @@ int RELAY2 = D4;
 int RELAY3 = D5;
 int RELAY4 = D6;
 
+int PINS[] = {D3, D4, D5, D6};
 int STATES[] = {0, 0, 0, 0};
 
 int ttl = 16777215;
 
-void callback(char * _topic, byte *payload, unsigned int length)
+void callback(char *_topic, byte *payload, unsigned int length)
 {
     char p[length + 1];
     memcpy(p, payload, length);
     p[length] = NULL;
 
-    String topic = String(_topic);
-    String relay_number = String(topic.charAt(topic.length() - 1));
-
     Particle.publish("event_callback", "handing payload: " + String(p), ttl);
-    String event = "relay_number: " + relay_number + ", payload: " + String(p);
-    client.publish(String(EVENTS_TOPIC_TEMPLATE) + "1", event);
-    client.publish(String(EVENTS_TOPIC_TEMPLATE) + "1", "{}");
+
+    String topic = String(_topic);
+    int relayNumber = topic.charAt(topic.length() - 1) - '0';
+
+    if (String("ON").equals(p))
+    {
+        setState(relayNumber, HIGH);
+        client.publish(String(EVENTS_TOPIC_TEMPLATE) + String(relayNumber), "ON");
+    }
+    else if (String("OFF").equals(p))
+    {
+        setState(relayNumber, LOW);
+        client.publish(String(EVENTS_TOPIC_TEMPLATE) + String(relayNumber), "OFF");
+    }
 }
 
 void reset_handler()
@@ -52,68 +62,15 @@ void reset_handler()
     Particle.publish("reset", "going down for reboot NOW!", ttl);
 }
 
-int getRelayNumber(String command)
+int setState(int relayNumber, int relayState)
 {
-    // parse the relay number
-    int relayNumber = command.charAt(1) - '0';
-    // do a sanity check
-    if (relayNumber < 1 || relayNumber > 4)
-        return -1;
-    else
-        return relayNumber;
-}
-
-/*
-command format r1,HIGH
-
-POST /v1/devices/{DEVICE_ID}/relay
-
-# EXAMPLE REQUEST
-curl https://api.particle.io/v1/devices/0123456789abcdef/relay \
--d access_token=123412341234 -d params=r1,HIGH
-*/
-int relayControl(String command)
-{
-    int relayState = 0;
-    // parse the relay number
-    int relayNumber = getRelayNumber(command);
-    if (relayNumber == -1)
-        return -1;
-
-    // find out the state of the relay
-    if (command.substring(3, 7) == "HIGH")
-        relayState = 1;
-    else if (command.substring(3, 6) == "LOW")
-        relayState = 0;
-    else
-        return -1;
-
     // write to the appropriate relay
-    digitalWrite(relayNumber + 2, relayState);
+    digitalWrite(PINS[relayNumber - 1], relayState);
 
     // update states variable
     STATES[relayNumber - 1] = relayState;
 
-    return 1;
-}
-
-/*
-command format r1
-
-POST /v1/devices/{DEVICE_ID}/state
-
-# EXAMPLE REQUEST
-curl https://api.particle.io/v1/devices/0123456789abcdef/state \
--d access_token=123412341234 -d params=r1
-*/
-int relayState(String args)
-{
-    // parse the relay number
-    int relayNumber = getRelayNumber(args);
-    if (relayNumber == -1)
-        return -1;
-
-    return STATES[relayNumber - 1];
+    return 0;
 }
 
 void ready()
@@ -130,21 +87,11 @@ void setup()
     // register the reset handler
     System.on(reset, reset_handler);
 
-    //Initilize the relay control pins as output
-    pinMode(RELAY1, OUTPUT);
-    pinMode(RELAY2, OUTPUT);
-    pinMode(RELAY3, OUTPUT);
-    pinMode(RELAY4, OUTPUT);
-
-    // Initialize all relays to an OFF state
-    digitalWrite(RELAY1, LOW);
-    digitalWrite(RELAY2, LOW);
-    digitalWrite(RELAY3, LOW);
-    digitalWrite(RELAY4, LOW);
-
-    // register functions
-    Particle.function("relay", relayControl);
-    Particle.function("state", relayState);
+    for (int pin : PINS)
+    {
+        pinMode(pin, OUTPUT);
+        digitalWrite(pin, LOW);
+    }
 
     // // connect to the mqtt broker
     client.connect("sparkclient");
@@ -154,7 +101,7 @@ void setup()
     {
         Particle.publish("MQTT: connected", PRIVATE);
 
-        client.publish(String(EVENTS_TOPIC_TEMPLATE)+"1", "Online.");
+        client.publish(String(EVENTS_TOPIC_TEMPLATE) + "general", "Particle relay hub came online.");
         client.subscribe(COMMANDS_TOPICS);
     }
 
