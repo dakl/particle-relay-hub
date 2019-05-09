c341283107a239059b4582b88dcc27c7dea54d73
Daniel Klevebring <daniel.klevebring@gmail.com>
2019-05-09 20:39:35 +0200

    add control endpoint

--- firmware/src/particle-relay-hub.ino
+++ firmware/src/particle-relay-hub.ino
@@ -26,6 +26,8 @@ char *EVENTS_TOPIC_TEMPLATE = "events/relay/";
 int PINS[] = {D3, D4, D5, D6};
 int STATES[] = {0, 0, 0, 0};
 
+int FAILED_TO_PARSE_PIN = -100;
+int FAILED_TO_PARSE_STATE = -200;
 int ttl = 16777215;
 
 void callback(char *_topic, byte *payload, unsigned int length)
@@ -73,6 +75,49 @@ void ready()
     Particle.publish("booted", "particle-relay-hub is ready.", ttl);
 }
 
+int getRelayNumber(String args)
+{
+    Particle.publish("get_pin", "Getting pin index from args " + args, ttl);
+    // parse the relay number
+    int pin = args.charAt(0) - '0';
+    Particle.publish("get_pin", "Pin was " + String(pin) , ttl);    
+    // do a sanity check
+    if (pin < 1 || pin > 4) return -1;
+    else return pin;
+}
+
+int getTargetState(String args)
+{
+    unsigned int pin, value;    
+    int n = sscanf(args, "%u,%u", &pin, &value);
+    Particle.publish("got_value", "value is " + String(value), ttl);    
+    // do a sanity check
+    if (value < 0 || value > 1) return -1;
+    else return value;
+}
+
+/*
+command format: pin,state[0=LOW, 1=HIGH]
+examples: `1,1`, `4,0`
+POST /v1/devices/{DEVICE_ID}/pin
+# EXAMPLE REQUEST
+curl https://api.particle.io/v1/devices/0123456789abcdef/pin \
+-d access_token=123412341234 -d params=1,1
+*/
+int pinControl(String args)
+{
+    Particle.publish("set_state", "Setting state using args " + args, ttl);
+    // parse the relay number
+    int targetRelay = getRelayNumber(args);
+    if (targetRelay == -1) return FAILED_TO_PARSE_PIN;
+
+    // find out the state of the relay
+    int targetState = getTargetState(args);
+    if (targetState == -1) return FAILED_TO_PARSE_STATE;
+    setState(targetRelay, targetState);
+    return 1;
+ }
+
 void setup()
 {
     Particle.publish("booting", "particle-relay-hub is setting up.", ttl);
@@ -86,7 +131,10 @@ void setup()
         digitalWrite(pin, LOW);
     }
 
-    // // connect to the mqtt broker
+    // register control function
+    Particle.function("pin", pinControl);
+
+    // connect to the mqtt broker
     client.connect("relay-hub");
 
     // publish/subscribe
